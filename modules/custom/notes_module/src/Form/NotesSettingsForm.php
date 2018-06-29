a63fba2235eb51899ca20c4f6515f89b99ea736c
Dmitry <dmtrmahrov@gmail.com>
2018-06-29 17:28:42 +0600

    Notes module vers 0.2

---
+++ modules/custom/notes_module/src/Form/NotesSettingsForm.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\notes_module\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure notes settings.
+ */
+class NotesSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'notes_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'notes.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('notes.settings');
+
+    $form['status'] = [
+      '#type' => 'checkbox',
+      '#value' => $this->t('Change status of all notes N/A'),
+    ];
+
+    $form['or'] = [
+      '#type' => 'item',
+      '#markup' => '<hr><h1> OR </h1>',
+    ];
+
+    $form['date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Choose actual date'),
+      '#default_value' => $config->get('date'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->configFactory->getEditable('notes.settings')
+      ->set('status', $form_state->getValue('status'))
+      ->set('date', $form_state->getValue('date'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
+
